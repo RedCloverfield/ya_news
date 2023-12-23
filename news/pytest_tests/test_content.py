67b8ea7fab05ec958709b79c1bd1d5478d205cee
Станислав Ефимов <stanislav.karov@mail.ru>
2023-12-23 15:17:18 +0300

    Create content pytest tests

---
+++ news/pytest_tests/test_content.py
@@ -0,0 +1,51 @@
+from django.conf import settings
+from django.urls import reverse
+import pytest
+
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.mark.usefixtures('list_of_news')
+def test_news_count_on_home_page(client):
+    home_url = reverse('news:home')
+    response = client.get(home_url)
+    object_list = response.context['object_list']
+    news_count = len(object_list)
+    assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
+
+
+@pytest.mark.usefixtures('list_of_news')
+def test_news_order(client):
+    home_url = reverse('news:home')
+    response = client.get(home_url)
+    object_list = response.context['object_list']
+    all_dates = [news.date for news in object_list]
+    sorted_dates = sorted(all_dates, reverse=True)
+    assert sorted_dates == all_dates
+
+
+@pytest.mark.usefixtures('list_of_comments')
+def test_comments_order(client, news, pk_for_args):
+    url = reverse('news:detail', args=(pk_for_args))
+    response = client.get(url)
+    assert 'news' in response.context
+    all_comments = news.comment_set.all()
+    commets_dates = [comment.created for comment in all_comments]
+    sorted_comments = sorted(commets_dates)
+    assert sorted_comments == commets_dates
+
+
+@pytest.mark.parametrize(
+    'user_client, form_status',
+    (
+        (pytest.lazy_fixture('client'), False),
+        (pytest.lazy_fixture('author_client'), True)
+    )
+)
+def test_comment_form_availability_for_different_users(
+    user_client, form_status, pk_for_args
+):
+    url = reverse('news:detail', args=(pk_for_args))
+    response = user_client.get(url)
+    assert ('form' in response.context) is form_status
